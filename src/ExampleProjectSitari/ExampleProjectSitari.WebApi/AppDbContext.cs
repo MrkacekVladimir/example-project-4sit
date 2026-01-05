09e33452f60abc50ca248dd10013de19d03edaa8
MrkacekVladimir <vlad.mrkacek@gmail.com>
2026-01-05 11:06:03 +0100

    taky udelam commit

---
+++ src/ExampleProjectSitari/ExampleProjectSitari.WebApi/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExampleProjectSitari.WebApi
+{
+    public class AppDbContext: DbContext
+    {
+    }
+}
---
+++ src/ExampleProjectSitari/ExampleProjectSitari.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleProjectSitari.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly AppDbContext dbContext;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(AppDbContext dbContext, ILogger<WeatherForecastController> logger)
+        {
+            this.dbContext = dbContext;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
